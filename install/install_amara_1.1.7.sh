5b24af5ac6dd47cb5eb84adcd4e5b8e3a3e599a0
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2008-06-04 11:46:40 +0000

    TJ: reapplied license.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-harvester/trunk@938 83e7a582-4515-4195-8f39-222cbda6297e

--- install/install_amara_1.1.7.sh
+++ install/install_amara_1.1.7.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 ## begin license ##
 #
-#    "Sahara" consists of two subsystems, namely an OAI-harvester and a web-control panel.
-#    "Sahara" is developed for SURFnet by:
+#    "Meresco Harvester" consists of two subsystems, namely an OAI-harvester and
+#    a web-control panel.
+#    "Meresco Harvester" is originally called "Sahara" and was developed for 
+#    SURFnet by:
 #        Seek You Too B.V. (CQ2) http://www.cq2.nl
-#    Copyright (C) 2006,2007 SURFnet B.V. http://www.surfnet.nl
+#    Copyright (C) 2006-2007 SURFnet B.V. http://www.surfnet.nl
+#    Copyright (C) 2007-2008 Seek You Too (CQ2) http://www.cq2.nl
+#    Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
+#    Copyright (C) 2007-2008 Stichting Kennisnet Ict op school.
+#       http://www.kennisnetictopschool.nl
 #
-#    This file is part of "Sahara"
+#    This file is part of "Meresco Harvester"
 #
-#    "Sahara" is free software; you can redistribute it and/or modify
+#    "Meresco Harvester" is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
 #    (at your option) any later version.
 #
-#    "Sahara" is distributed in the hope that it will be useful,
+#    "Meresco Harvester" is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 #
 #    You should have received a copy of the GNU General Public License
-#    along with "Sahara"; if not, write to the Free Software
+#    along with "Meresco Harvester"; if not, write to the Free Software
 #    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 ## end license ##
