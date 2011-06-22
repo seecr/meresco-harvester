899a7a8b66eb23f46932055280766073d91278af
thijsj <thijsj@83e7a582-4515-4195-8f39-222cbda6297e>
2011-06-22 14:02:43 +0000

    TJ: test setup.
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-harvester/trunk@3702 83e7a582-4515-4195-8f39-222cbda6297e

---
+++ testsetup.sh
@@ -0,0 +1,68 @@
+## begin license ##
+# 
+# "Meresco Harvester" consists of two subsystems, namely an OAI-harvester and
+# a web-control panel.
+# "Meresco Harvester" is originally called "Sahara" and was developed for 
+# SURFnet by:
+# Seek You Too B.V. (CQ2) http://www.cq2.nl 
+# 
+# Copyright (C) 2010-2011 Seek You Too (CQ2) http://www.cq2.nl
+# Copyright (C) 2010-2011 Stichting Kennisnet http://www.kennisnet.nl
+# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# 
+# This file is part of "Meresco Harvester"
+# 
+# "Meresco Harvester" is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# "Meresco Harvester" is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with "Meresco Harvester"; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# 
+## end license ##
+
+set -e
+mydir=$(cd $(dirname $0); pwd)
+
+rm -rf tmp build
+
+python setup.py install --root tmp
+cp -r test tmp/test
+cp meresco/__init__.py tmp/usr/lib/python2.5/site-packages/meresco
+find tmp -name '*.py' -exec sed -e \
+    "/DO_NOT_DISTRIBUTE/d;
+    s,^examplesPath.*$,examplesPath='$mydir/examples',;
+    s,^binDir.*$,binDir='$mydir/tmp/usr/bin'," -i {} \;
+
+export PYTHONPATH=`pwd`/tmp/usr/lib/python2.5/site-packages
+teststorun=$1
+if [ -z "$teststorun" ]; then
+    teststorun="alltests.sh integrationtest.sh"
+else
+    shift
+fi
+
+echo "Will now run the tests:"
+for f in $teststorun; do
+    echo "- $f"
+done
+echo "Press [ENTER] to continue"
+read
+
+for testtorun in $teststorun; do
+    (
+    cd tmp/test
+    ./$testtorun "$@"
+    )
+    echo "Finished $testtorun, press [ENTER] to continue"
+    read
+done
+
+rm -rf tmp build
