119473e89b2940e3f425e47de88c900f28208c55
jurjanpaul <jurjanpaul@83e7a582-4515-4195-8f39-222cbda6297e>
2012-08-14 09:20:58 +0000

    HM/JPM: applied license
    git-svn-id: file:///home/thijs/development/meresco2git/meresco/meresco-harvester/workingsets/8.1.4.1-Edurep/version_0-first-partial-responsedate-as-from@4564 83e7a582-4515-4195-8f39-222cbda6297e

--- test/harvesterlogtest.py
+++ test/harvesterlogtest.py
@@ -9,11 +9,10 @@
 # Copyright (C) 2006-2007 SURFnet B.V. http://www.surfnet.nl
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2011 Seek You Too (CQ2) http://www.cq2.nl
-# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
-# Copyright (C) 2010-2011 Stichting Kennisnet http://www.kennisnet.nl
-# 
+# Copyright (C) 2010-2012 Stichting Kennisnet http://www.kennisnet.nl
+# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
 # 
 # This file is part of "Meresco Harvester"
 # 
