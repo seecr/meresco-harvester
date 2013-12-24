7214f8a60a5fb35a05bda9dd666ecf5bd23adea6
Seecr Development Team <development@seecr.nl>
2013-12-24 11:35:18 +0100

    HM/TJ: repository.edit is now based on lxml.

--- meresco/harvester/controlpanel/merescoharvesterhandler.py
+++ meresco/harvester/controlpanel/merescoharvesterhandler.py
@@ -1,35 +1,35 @@
 ## begin license ##
-# 
+#
 # "Meresco Harvester" consists of two subsystems, namely an OAI-harvester and
 # a web-control panel.
-# "Meresco Harvester" is originally called "Sahara" and was developed for 
+# "Meresco Harvester" is originally called "Sahara" and was developed for
 # SURFnet by:
-# Seek You Too B.V. (CQ2) http://www.cq2.nl 
-# 
+# Seek You Too B.V. (CQ2) http://www.cq2.nl
+#
 # Copyright (C) 2006-2007 SURFnet B.V. http://www.surfnet.nl
 # Copyright (C) 2007-2008 SURF Foundation. http://www.surf.nl
 # Copyright (C) 2007-2009, 2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2007-2009 Stichting Kennisnet Ict op school. http://www.kennisnetictopschool.nl
 # Copyright (C) 2009 Tilburg University http://www.uvt.nl
-# Copyright (C) 2011 Seecr (Seek You Too B.V.) http://seecr.nl
+# Copyright (C) 2011, 2013 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2011 Stichting Kennisnet http://www.kennisnet.nl
-# 
+#
 # This file is part of "Meresco Harvester"
-# 
+#
 # "Meresco Harvester" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Harvester" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Harvester"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from slowfoot.slowfoothandler import Request, handlepsp, util, _psp_login, retrieveSession, apache
@@ -37,6 +37,7 @@ from disallowfileplugin import DisallowFilePlugin
 from urllib import urlencode
 from urllib2 import urlopen
 from os.path import dirname, join
+from meresco.harvester.namespaces import xpath, xpathFirst
 
 templatesPath = join(dirname(__file__), 'slowfoottemplates')
 
@@ -85,7 +86,12 @@ def handler(req):
 
     req.get_options()['templatesPath'] = templatesPath
     req.get_options().setdefault('localhostUrl', 'http://localhost')
-    req = Request(req, handlepsp, util, _psp_login, retrieveSession)
+    req = Request(req, handlepsp, util, _psp_login, retrieveSession, 
+        additionalGlobals=dict(
+                xpath=xpath,
+                xpathFirst=xpathFirst,
+            )
+        )
     
     req.addPlugin('PREOPEN', dfp)
     req.go()
