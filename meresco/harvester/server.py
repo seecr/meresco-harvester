8db411cc4178fac1730f88ef1f145cd1783303d6
Seecr Development Team <development@seecr.nl>
2015-07-10 11:04:24 +0200

    HM: Use javascript libraries from seecr-web-lib

--- meresco/harvester/server.py
+++ meresco/harvester/server.py
@@ -1,32 +1,32 @@
 #!/usr/bin/env python2.5
 ## begin license ##
-# 
+#
 # "Meresco Harvester" consists of two subsystems, namely an OAI-harvester and
 # a web-control panel.
-# "Meresco Harvester" is originally called "Sahara" and was developed for 
+# "Meresco Harvester" is originally called "Sahara" and was developed for
 # SURFnet by:
-# Seek You Too B.V. (CQ2) http://www.cq2.nl 
-# 
-# Copyright (C) 2011-2012 Seecr (Seek You Too B.V.) http://seecr.nl
+# Seek You Too B.V. (CQ2) http://www.cq2.nl
+#
+# Copyright (C) 2011-2012, 2015 Seecr (Seek You Too B.V.) http://seecr.nl
 # Copyright (C) 2011 Seek You Too (CQ2) http://www.cq2.nl
 # Copyright (C) 2011-2012 Stichting Kennisnet http://www.kennisnet.nl
-# 
+#
 # This file is part of "Meresco Harvester"
-# 
+#
 # "Meresco Harvester" is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 2 of the License, or
 # (at your option) any later version.
-# 
+#
 # "Meresco Harvester" is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with "Meresco Harvester"; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+#
 ## end license ##
 
 from os.path import join, abspath, dirname
@@ -40,21 +40,20 @@ from weightless.core import compose, be
 
 from meresco.core import Observable
 
-from meresco.html import DynamicHtml
+from seecr.html import DynamicHtml
+from seecr.weblib import seecrWebLibPath
 
 from meresco.components import readConfig
-from meresco.components.http import ApacheLogger, PathFilter, ObservableHttpServer, StringServer, FileServer
+from meresco.components.http import ApacheLogger, PathFilter, ObservableHttpServer, StringServer, FileServer, PathRename
 from meresco.components.http.utils import ContentTypePlainText
 
 from __version__ import VERSION_STRING
-from saharaget import SaharaGet
 from repositorystatus import RepositoryStatus
 from harvesterdata import HarvesterData
 
 
 myPath = dirname(abspath(__file__))
 dynamicHtmlPath = join(myPath, 'controlpanel', 'html', 'dynamic')
-staticHtmlPath = join(myPath, 'controlpanel', 'html')
 
 def dna(reactor, observableHttpServer, config):
     harvesterData = HarvesterData(config["dataPath"])
@@ -66,7 +65,9 @@ def dna(reactor, observableHttpServer, config):
                         (StringServer(VERSION_STRING, ContentTypePlainText), )
                     ),
                     (PathFilter("/static"),
-                        (FileServer(staticHtmlPath),)
+                        (PathRename(lambda name: name[len('/static/'):]),
+                            (FileServer(seecrWebLibPath),)
+                        )
                     ),
                     (PathFilter('/', excluding=['/info/version', '/static']),
                         (DynamicHtml(
